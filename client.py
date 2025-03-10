c5f57bf27912979a00c80892d12b0de4e300da0e
Meile5 <meile.kapisaite5@gmail.com>
2025-03-10 11:31:13 +0100

    added language translator, but not yet working when testing with uviron

--- client.py
+++ client.py
@@ -21,6 +21,6 @@ async def saveItem(item: Item):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+#if __name__ == '__main__':
+   # import uvicorn
+   # uvicorn.run(app, host='0.0.0.0', port=8080)
